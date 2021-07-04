7c63bfa54259859132c0c330dd0fcf18f8aacdb4
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-07-04 04:52:40 +0300

    fixed

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pangrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string result = pangrams(s);
+
+            Console.WriteLine(result);
+        }
+
+        private static string pangrams(string s)
+        {
+            s = s.Replace(" ", string.Empty);
+            HashSet<char> myAlphapet = new HashSet<char>();
+
+            foreach (var item in s.ToLower())
+            {
+                myAlphapet.Add(item);
+
+            }
+            if (myAlphapet.Count == 26)
+            {
+                return "pangram";
+            }
+            else
+            {
+                return "not pangram";
+            }
+        }
+    }
+}
